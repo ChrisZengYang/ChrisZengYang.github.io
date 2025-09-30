340dc81ad200a6d500f59605455731faac549344
N1tr1c <dudecat41@gmail.com>
2025-09-30 13:47:34 -0400

    a

--- minecraft/initialize.js
+++ minecraft/initialize.js
@@ -16,9 +16,17 @@ document.addEventListener("DOMContentLoaded", () => {
     `);
 
     //Put the back links (to go back to main page)
-    document.body.querySelector("h1").insertAdjacentHTML("beforebegin", `
-        <a href="./" class="back-button"><small>Back</small></a>
-    `);
+    let path = window.location.pathname.split("/");
+
+    if(path[path.length-1] !== "index.html") { //NO BACK LINKS NEEDED FOR HOME PAGE!!!!! (except the other index.html)
+        document.body.querySelector("h1").insertAdjacentHTML("beforebegin", `
+            <a href="./" class="back-button"><small>Back</small></a>
+        `);
+    } else{
+        document.body.querySelector("h1").insertAdjacentHTML("beforebegin", `
+            <a href=".." class="back-button"><small>Back to chriszengyang.github.io</small></a>
+        `);
+    }
 
     //Put a nice horizontal bar for the heck of it after the title
     document.body.querySelector("h1").insertAdjacentHTML("afterend", `
