d38dcb8d13855c54cee873b1dae90ec9f0e6c8d3
N1tr1c <dudecat41@gmail.com>
2025-11-23 16:55:32 -0500

    Changed navlinks to navLinks

--- antrawarehouse/script.js
+++ antrawarehouse/script.js
@@ -1,3 +1,4 @@
+//Highlight current page in navigation bar
 let path = window.location.pathname.split("/");
 let currentPath = "/" + path.slice(1, path.length-1).join("/") + "/";
 let navLinks = document.querySelectorAll("nav ul li a");
@@ -7,3 +8,24 @@ navLinks.forEach(link => {
         link.parentElement.classList.add("active-link");
     }
 });
+
+
+//Add cool fade-in effect to landing page headers
+function fadeInOnScroll() {
+    const elements = document.querySelectorAll('.product-card, h1, p');
+    elements.forEach(element => {
+        const rect = element.getBoundingClientRect();
+        
+        if (window.scrollY + window.innerHeight > rect.top + window.scrollY) {
+            element.style.opacity = "1";
+            element.style.animation = 'fadeIn 1s ease forwards';
+        } else {
+            element.style.opacity = "0";
+            element.style.animation = 'none';
+        }
+    });
+}
+
+window.addEventListener('DOMContentLoaded', fadeInOnScroll); //First initial scroll check
+window.addEventListener('scroll', fadeInOnScroll);
+console.log(3);
