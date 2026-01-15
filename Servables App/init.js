2135c5ce6fd32889f00c37b313fac5ff41c7e8e7
N1tr1c <dudecat41@gmail.com>
2026-01-14 19:06:06 -0500

    Servables App Added

---
+++ Servables App/init.js
@@ -0,0 +1,26 @@
+let host;
+
+if(window.location.protocol === "file:") {
+    host = "file://" + "C:/users/chris%20yang/source/repos/ChrisZengYang.github.io/servables%20app";
+} else{
+    host = window.location.hostname;
+}
+
+const nav = `
+    <nav>
+        <ul class="left">
+            <li>
+                <a href="${host}"><img class="logo" src="${host}/resources/Servables.png" alt="Servables"></a>
+            </li>
+            <li style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
+                <a>Manage Servers</a>
+            </li>
+        </ul>
+        <ul class="right">
+            <button onclick="window.location.href='${host}/sign-up/'">Sign Up</button>
+            <button class="accent1" onclick="window.location.href='${host}/log-in/'">Log In</button>
+        </ul>
+    </nav>
+`;
+
+document.body.insertAdjacentHTML("afterbegin", nav);
